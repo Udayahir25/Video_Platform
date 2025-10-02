e25153604738b8c5474acdf780ba992349994287
Udayahir25 <udaynandaniya987@gmail.com>
2025-10-02 15:48:23 +0530

    NOTHING

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
+
+
+
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
+      // `${process.env.MONGODB_URI}`
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
